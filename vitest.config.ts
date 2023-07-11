7b4b43f193aa60d7faae1fd1dc95c5a93f99abe1
Asnow <--global>
2023-07-11 19:11:07 +0800

    init

---
+++ vitest.config.ts
@@ -0,0 +1,12 @@
+import { defineConfig } from "vitest/config";
+
+export default defineConfig({
+    test: {
+        alias: {
+            "#rt": __dirname + "/src",
+        },
+        coverage: {
+            exclude: ["**/__mocks__","**/*errors.ts"],
+        },
+    },
+});
