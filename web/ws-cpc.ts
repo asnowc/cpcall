2b505c87820693793188f1f64c4efe34624a4e54
Asnow <--global>
2023-08-07 21:22:30 +0800

    feat: WebSocketCpc

---
+++ web/ws-cpc.ts
@@ -0,0 +1,34 @@
+import { CpcFrame, FrameCpc } from "#rt/cpcp/frame_cpc.js";
+import { JBSON, toArrayJBSON } from "#rt/common/js_bson.js";
+import { CpcCmdList } from "#rt/cpc/cpc_frame.type.js";
+
+export class WebSocketCpc<
+    CallableCmd extends CpcCmdList = CpcCmdList,
+    CmdList extends CpcCmdList = CpcCmdList
+> extends FrameCpc<CallableCmd, CmdList> {
+    static createConnect(url: string | URL, protocols?: string | string[]) {
+        return new Promise<WebSocketCpc>(function (resolve, reject) {
+            const socket = new WebSocket(url, protocols);
+            socket.binaryType = "arraybuffer";
+            socket.onerror = reject;
+            socket.onopen = () => resolve(new WebSocketCpc(socket));
+        });
+    }
+    private constructor(private socket: WebSocket) {
+        super();
+        socket.onmessage = this.onMsg;
+        socket.onerror = (e) => this.emit("error", e);
+        socket.onclose = (e) => this.dispose();
+    }
+    private onMsg = (event: MessageEvent<ArrayBuffer>) => {
+        const buf = event.data;
+        if (buf instanceof ArrayBuffer) {
+            let offset = 0;
+            let frame = JBSON.toArray<CpcFrame>(buf, offset);
+            this.onCpcFrame(frame);
+        }
+    };
+    protected sendFrame(frame: CpcFrame): void {
+        this.socket.send(toArrayJBSON(frame));
+    }
+}
