3f12e97345d071691959705c8974a3a00ad37d80
Eavid <ganrn@vip.qq.com>
2024-01-09 22:46:26 +0800

    chore: 构建、测试配置更改

--- rollup.config.js
+++ rollup.config.js
@@ -2,33 +2,37 @@
 import { defineEvConfig } from "@eavid/lib-dev/rollup";
 import * as path from "node:path";
 
-const dir = process.cwd()
+const dir = process.cwd();
 console.log("cwd:" + dir);
-const libDir = path.resolve(dir, "src/cpc/lib")
+const libDir = path.resolve(dir, "src/cpc/lib");
 
 export default defineEvConfig({
-    input: {
-        "cpc": "src/cpc/cpc.ts",
-        "node": "src/node/node.ts",
-        "web": "src/web/web.ts"
+  input: {
+    cpc: "src/cpc/cpc.ts",
+    node: "src/node/node.ts",
+    web: "src/web/web.ts",
+  },
+  output: {
+    dir: "dist",
+    chunkFileNames: "internal/[name].js",
+    minifyInternalExports: false,
+    entryFileNames: "[name]/[name].js",
+    manualChunks(id, meta) {
+      if (id.startsWith(libDir)) return "lib";
     },
-    output: {
-        dir: "dist",
-        chunkFileNames: "internal/[name].js",
-        minifyInternalExports: false,
-        entryFileNames: "[name]/[name].js",
-        manualChunks(id, meta) {
-            if (id.startsWith(libDir)) return "lib"
-        }
+  },
+  extra: {
+    typescript: {
+      compilerOptions: {
+        module: "NodeNext",
+        rootDir: "src",
+        outDir: "./dist",
+        noEmit: false,
+        declaration: true,
+        declarationMap: true,
+        lib: ["ESNext", "dom"],
+      },
     },
-    extra: {
-        typescript: {
-            module: "NodeNext",
-            rootDir: "src",
-            outDir: "./dist",
-            noEmit: false,
-            declaration: true,
-            declarationMap: true
-        }, resolve: {}
-    }
+    resolve: {},
+  },
 });
--- vitest.config.ts
+++ vitest.config.ts
@@ -2,7 +2,6 @@ import { defineConfig } from "vitest/config";
 import * as path from "node:path";
 
 const root = __dirname;
-const packagesRoot = path.resolve(root, "../");
 
 export default defineConfig({
   test: {
@@ -10,7 +9,6 @@ export default defineConfig({
       { find: /^#lib/, replacement: path.resolve(root, "src/cpc/lib") },
       { find: /^cpcall$/, replacement: path.resolve(root, "src/cpc/cpc.js") },
       { find: /^cpcall\/node$/, replacement: path.resolve(root, "src/node/node.js") },
-      { find: /^@eavid\/js-bson$/, replacement: path.resolve(packagesRoot, "js-bson/src/js_bson.js") },
     ],
     coverage: {
       exclude: ["**/__mocks__", "**/*errors.ts", "**/*.type.ts", "**/lib/**"],
