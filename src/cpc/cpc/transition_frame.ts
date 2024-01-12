40204f851581bc76eba32b855dd2e430ad425174
Eavid <--global>
2024-01-12 19:34:57 +0800

    feat: 引用 jbod

--- src/cpc/cpc/transition_frame.ts
+++ src/cpc/cpc/transition_frame.ts
@@ -1,95 +1,99 @@
-import { DataType, JBSON, VOID, toArrayJBSON, toArrayItemJBSON, DLD, numToDLD } from "@eavid/js-bson";
-import { CpcFrame, CpcUnknownFrameTypeError, FrameType } from "./cpc.js";
+import { CpcFrame, CpcUnknownFrameTypeError, FrameType, VOID } from "./cpc.js";
+import JBOD, { DBN, DataType } from "jbod";
 
 export function readCpcFrame(frame: Uint8Array): CpcFrame {
-    const type: FrameType = frame[0];
-    if (type === FrameType.call || type === FrameType.exec) {
-        const args = JBSON.toArray(frame, 1);
-        return [type, args];
-    } else if (type === FrameType.reject || type === FrameType.resolve) {
-        const [id, len] = DLD.readNumberSync(frame, 1);
-        const [value] = JBSON.toArrayItem<string>(frame, len + 1);
-        return [type, id, value];
-    } else {
-        switch (type) {
-            case FrameType.return: {
-                const [value] = JBSON.toArrayItem(frame, 1);
-                return [type, value];
-            }
-            case FrameType.throw: {
-                if (frame[1] === DataType.void) return [type, VOID];
+  const type: FrameType = frame[0];
+  if (type === FrameType.call || type === FrameType.exec) {
+    const args = JBOD.parse<unknown[]>(frame.subarray(1), DataType.array).data;
+    return [type, args];
+  } else if (type === FrameType.reject || type === FrameType.resolve) {
+    const [id, len] = DBN.paseNumberSync(frame, 1);
+    const value = JBOD.parse(frame.subarray(len + 1)).data;
+    return [type, id, value];
+  } else {
+    switch (type) {
+      case FrameType.return: {
+        const value = JBOD.parse(frame.subarray(1)).data;
+        return [type, value];
+      }
+      case FrameType.throw: {
+        if (frame[1] === DataType.void) return [type, VOID];
 
-                const [value] = JBSON.toArrayItem(frame, 1);
-                return [type, value];
-            }
-            case FrameType.returnAsync: {
-                const [id] = DLD.readNumberSync(frame, 1);
-                return [type, id];
-            }
-            case FrameType.fin:
-                return [FrameType.fin];
-            default:
-                throw new CpcUnknownFrameTypeError(type);
-        }
+        const value = JBOD.parse(frame.subarray(1)).data;
+        return [type, value];
+      }
+      case FrameType.returnAsync: {
+        const [id] = DBN.paseNumberSync(frame, 1);
+        return [type, id];
+      }
+      case FrameType.fin:
+        return [FrameType.fin];
+      default:
+        throw new CpcUnknownFrameTypeError(type);
     }
+  }
 }
 
 export function sendCpcFrame(frame: CpcFrame): [Uint8Array[], number] {
-    const type = frame[0];
-    const writeList: Uint8Array[] = [createTypeFlagBuf(type)];
-    let contentLen = 1;
-    if (type === FrameType.call || type === FrameType.exec) {
-        const argBuf = toArrayJBSON(frame[1], true);
-        writeList.push(argBuf);
-        contentLen += argBuf.byteLength;
-    } else if (type === FrameType.reject || type === FrameType.resolve) {
-        const idBuf = numToDLD(frame[1]);
-        const valueBuf = toArrayItemJBSON(frame[2]);
-        writeList.push(idBuf); //id
+  const type = frame[0];
+  const writeList: Uint8Array[] = [createTypeFlagBuf(type)];
+  let contentLen = 1;
+  if (type === FrameType.call || type === FrameType.exec) {
+    const argBuf = JBOD.binaryifyContent(frame[1]);
+    writeList.push(argBuf);
+    contentLen += argBuf.byteLength;
+  } else if (type === FrameType.reject || type === FrameType.resolve) {
+    const idBuf = DBN.numToBinary(frame[1]);
+    const valueBuf = JBOD.binaryifyContent(frame[2]);
+    writeList.push(new Uint8Array([JBOD.getType(frame[2])]));
+    writeList.push(idBuf); //id
+    writeList.push(valueBuf); //value
+    contentLen += 1;
+    contentLen += idBuf.byteLength;
+    contentLen += valueBuf.byteLength;
+  } else {
+    switch (type) {
+      case FrameType.return: {
+        const valueBuf = JBOD.binaryifyContent(frame[1]);
+        writeList.push(new Uint8Array([JBOD.getType(frame[1])]));
         writeList.push(valueBuf); //value
-        contentLen += idBuf.byteLength;
-        contentLen += valueBuf.byteLength;
-    } else {
-        switch (type) {
-            case FrameType.return: {
-                const valueBuf = toArrayItemJBSON(frame[1]);
-                writeList.push(valueBuf); //value
-                contentLen += valueBuf.byteLength;
-                break;
-            }
-            case FrameType.throw: {
-                //isNoExist
-                if (frame[1] === VOID) {
-                    writeList.push(createTypeFlagBuf(DataType.void)); //write void
-                    contentLen += 1;
-                } else {
-                    const valueBuf = toArrayItemJBSON(frame[1]);
-                    writeList.push(valueBuf); //value
-                    contentLen += valueBuf.byteLength;
-                }
-                break;
-            }
-            case FrameType.returnAsync: {
-                const idBuf = numToDLD(frame[1]);
-                writeList.push(idBuf); //id
-                contentLen += idBuf.byteLength;
-                break;
-            }
+        contentLen += valueBuf.byteLength + 1;
+        break;
+      }
+      case FrameType.throw: {
+        //isNoExist
+        if (frame[1] === VOID) {
+          writeList.push(createTypeFlagBuf(DataType.void)); //write void
+          contentLen += 1;
+        } else {
+          const valueBuf = JBOD.binaryifyContent(frame[1]);
+          writeList.push(new Uint8Array([JBOD.getType(frame[1])]));
+          writeList.push(valueBuf); //value
+          contentLen += valueBuf.byteLength + 1;
         }
+        break;
+      }
+      case FrameType.returnAsync: {
+        const idBuf = DBN.numToBinary(frame[1]);
+        writeList.push(idBuf); //id
+        contentLen += idBuf.byteLength;
+        break;
+      }
     }
-    return [writeList, contentLen];
+  }
+  return [writeList, contentLen];
 }
 export function concatUint8ArrayList(list: Uint8Array[], totalLen: number) {
-    let frameBuf = new Uint8Array(totalLen);
-    let offset = 0;
-    for (let i = 0; i < list.length; i++) {
-        frameBuf.set(list[i], offset);
-        offset += list[i].byteLength;
-    }
-    return frameBuf;
+  let frameBuf = new Uint8Array(totalLen);
+  let offset = 0;
+  for (let i = 0; i < list.length; i++) {
+    frameBuf.set(list[i], offset);
+    offset += list[i].byteLength;
+  }
+  return frameBuf;
 }
 function createTypeFlagBuf(type: number) {
-    const t = new Uint8Array(1);
-    t[0] = type;
-    return t;
+  const t = new Uint8Array(1);
+  t[0] = type;
+  return t;
 }
--- test/__mocks__/bson.cases.ts
+++ test/__mocks__/bson.cases.ts
@@ -1,43 +1,36 @@
-import { ObjectId } from "@eavid/js-bson";
 import { vi, expect } from "vitest";
 
 export const baseDataTypes = {
-    noContent: [undefined, null, true, false],
-    int: [-2147483648, -66, -1, 0, 1, , 2147483647],
-    bigint: [-9223372036854775808n, , -1n, 0n, 1n, , 9223372036854775807n],
-    double: [-1.1, 1.1, NaN, Infinity, -Infinity],
-    string: ["abcd中文123", ""],
-    map: [{ a: 8, b: false, q: [1, 3, "a", null], c: { a: 9 } }],
-    array: [[1, 4, "2"]],
+  noContent: [undefined, null, true, false],
+  int: [-2147483648, -66, -1, 0, 1, , 2147483647],
+  bigint: [-9223372036854775808n, , -1n, 0n, 1n, , 9223372036854775807n],
+  double: [-1.1, 1.1, NaN, Infinity, -Infinity],
+  string: ["abcd中文123", ""],
+  map: [{ a: 8, b: false, q: [1, 3, "a", null], c: { a: 9 } }],
+  array: [[1, 4, "2"]],
 };
 export type ObjectDataType = { data: any[]; expect(rawData: any, data: any, msg?: string | number): void };
 export const objectDataTypes = {
-    arrayBuffer: {
-        data: [new ArrayBuffer(5)],
-        expect(rawData: ArrayBuffer, data: any) {
-            expect(data).instanceof(ArrayBuffer);
-            expect(Buffer.from(data).toString("hex")).toBe(Buffer.from(rawData).toString("hex"));
-        },
+  arrayBuffer: {
+    data: [new ArrayBuffer(5)],
+    expect(rawData: ArrayBuffer, data: any) {
+      expect(data).instanceof(ArrayBuffer);
+      expect(Buffer.from(data).toString("hex")).toBe(Buffer.from(rawData).toString("hex"));
     },
-    error: {
-        data: [new Error("abc"), new Error("abc", { cause: 23 })],
-        expect(rawData: Error, data: any) {
-            expect(data).instanceof(Error);
-            expect(rawData).toMatchObject(data);
-        },
+  },
+  error: {
+    data: [new Error("abc"), new Error("abc", { cause: 23 })],
+    expect(rawData: Error, data: any) {
+      expect(data).instanceof(Error);
+      expect(rawData).toMatchObject(data);
     },
-    objectId: {
-        data: [new ObjectId(88n), new ObjectId(0), new ObjectId(2 ** 32 + 1)],
-        expect(rawData: ObjectId, data: any, msg?: number | string) {
-            expect(data, msg?.toString()).toEqual(rawData);
-            expect(data, msg?.toString()).instanceof(ObjectId);
-        },
+  },
+
+  regExp: {
+    data: [/\d+./],
+    expect(rawData: RegExp, data: any) {
+      expect(data).instanceof(RegExp);
+      expect(data.source).toBe(rawData.source);
     },
-    regExp: {
-        data: [/\d+./],
-        expect(rawData: RegExp, data: any) {
-            expect(data).instanceof(RegExp);
-            expect(data.source).toBe(rawData.source);
-        },
-    },
+  },
 };
