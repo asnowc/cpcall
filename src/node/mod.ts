78ebebdee5a4dfba6a5bcfbbdddc61b208d2b650
Eavid <--global>
2024-02-27 13:10:26 +0800

    feat: node， web 包导出 Cpc 类型

--- src/node/mod.ts
+++ src/node/mod.ts
@@ -1,1 +1,8 @@
+export {
+  CpCall,
+  CpcFailAsyncRespondError,
+  CpcFailRespondError,
+  CpcUnregisteredCommandError,
+  type MakeCallers,
+} from "cpcall";
 export * from "./socket_cpc.js";
--- src/web/mod.ts
+++ src/web/mod.ts
@@ -1,2 +1,9 @@
+export {
+  CpCall,
+  CpcFailAsyncRespondError,
+  CpcFailRespondError,
+  CpcUnregisteredCommandError,
+  type MakeCallers,
+} from "cpcall";
 export * from "./web_socket.js";
 export * from "./stream.js";
