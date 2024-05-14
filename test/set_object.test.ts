1766c8ffd1d280b7511b873153ac44d029d86d3c
Eavid <ganrn@vip.qq.com>
2024-05-14 16:52:45 +0800

    fix(core): CpCall.setObject() 设置的对象存在循环引用时发生异常

---
+++ test/set_object.test.ts
@@ -0,0 +1,72 @@
+import { describe, test, expect, vi, beforeEach } from "vitest";
+import { RpcFrame, CpCall } from "cpcall";
+import { PassiveDataCollector } from "evlib/async";
+
+describe("setObject", function () {
+  const { cpc } = createCpc();
+
+  class Abc {
+    static cca() {}
+    constructor() {}
+    cf() {}
+    get yi() {
+      return () => {};
+    }
+  }
+  class Child extends Abc {
+    child() {
+      return this.att;
+    }
+    att = 9;
+  }
+  beforeEach(() => {
+    cpc.clearFn();
+  });
+  test("setSub", function () {
+    cpc.setObject({ abc: { def() {} } });
+    const expectSet = new Set(["abc.def"]);
+    setEq(cpc.getAllFn(), expectSet);
+  });
+  test("getter", function () {
+    cpc.setObject(new Child());
+    const expectSet = new Set(["cf", "yi", "child"]);
+    setEq(cpc.getAllFn(), expectSet);
+  });
+  test("map", function () {
+    expect(() => cpc.setObject(new Map())).not.toThrowError();
+  });
+  test("static", function () {
+    cpc.setObject(Child);
+    const expectSet = new Set(["cca"]);
+    setEq(cpc.getAllFn(), expectSet);
+  });
+  test("循环引用", function () {
+    const sub: any = {
+      a: () => {},
+    };
+    const sub2 = { s: sub };
+    sub.s = sub2;
+    const root = {
+      sub,
+      sub2,
+    };
+    cpc.setObject(root);
+    const expectSet = new Set(["sub.a", "sub2.s.a"]);
+    setEq(cpc.getAllFn(), expectSet);
+  });
+});
+
+function createCpc() {
+  const onSendFrame = vi.fn();
+  let hd = new PassiveDataCollector<RpcFrame>();
+  let cpc = new CpCall({ frameIter: hd.getAsyncGen(), sendFrame: onSendFrame });
+  return { onSendFrame, hd, cpc };
+}
+function setEq(s1: Iterable<any>, s2: Set<any>) {
+  let size: number = 0;
+  for (const s of s1) {
+    if (!s2.has(s)) throw new Error("不存在 " + s);
+    size++;
+  }
+  expect(size).toBe(s2.size);
+}
