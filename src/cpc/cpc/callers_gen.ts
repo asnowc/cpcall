92ce0239d06d8c25465c3d2f391d4d967defe424
Eavid <ganrn@vip.qq.com>
2024-05-21 23:10:36 +0800

    refactor: 类型更改

--- src/cpc/cpc/callers_gen.ts
+++ src/cpc/cpc/callers_gen.ts
@@ -8,16 +8,17 @@ class ChainProxyHandler implements ProxyHandler<ProxyFn> {
     if (typeof p !== "string") return value;
     if (value !== undefined) return value;
     // target 取不到值， 返回代理
-    const head: Chian = { value: p, next: Reflect.get(target, CALL_CHAIN) as any };
-    return createCallChain(this.#createProxy, head);
+
+    return createObjectChain(p, target, this.#createProxy);
   }
   apply(target: ProxyFn, thisArg: any, argArray: any[]) {
-    return target(target, argArray, thisArg);
+    return target(argArray, thisArg, target);
   }
 }
-type ProxyFn = (target: Fn, argArray: any[], thisArg: any) => any;
+type ProxyFn = (argArray: any[], thisArg: any, target: ProxyFn) => any;
 
-export function getChainPath(obj: Fn): string[] {
+/** 获取对象链上的路径 */
+export function getChainPath(obj: object): string[] {
   const chain = Reflect.get(obj, CALL_CHAIN);
   let strList: string[] = [];
   if (chain) {
@@ -30,12 +31,47 @@ export function getChainPath(obj: Fn): string[] {
   }
   return strList;
 }
-export function createCallChain<T extends Fn, E extends object = {}>(
-  createProxy?: () => T,
-  chain?: Chian
-): CallChianProxy<T, E> {
+/**
+ * @param createProxy 构造新的链时调用的函数，这个函数应返回一个新的对象
+ */
+export function createObjectChain<T extends ProxyFn = ProxyFn>(
+  createProxy?: () => T
+): CallChian<T>;
+/**
+ * 基于对象链继续扩展链
+ * @param key 扩展的键名
+ * @param extend 基于的对象链。如果不存在，key 将为默认键名
+ * @param createProxy 构造新的链时调用的函数，这个函数应返回一个新的对象
+ */
+export function createObjectChain<T extends ProxyFn = ProxyFn>(
+  key?: string,
+  extend?: object,
+  createProxy?: () => T
+): CallChian<T>;
+export function createObjectChain(
+  key_create?: (() => ProxyFn) | string,
+  extend?: object,
+  createProxy?: () => ProxyFn
+): CallChian<ProxyFn> {
+  let key: string | undefined;
+  if (typeof key_create === "function") createProxy = key_create;
+  else key = key_create;
+
+  let chain: Chian | undefined;
+  if (key) {
+    chain = {
+      value: key,
+      next: extend && Reflect.get(extend, CALL_CHAIN),
+    };
+  }
+
   const target = createProxy ? createProxy() : function () {};
-  Reflect.defineProperty(target, CALL_CHAIN, { configurable: false, writable: false, enumerable: false, value: chain });
+  Reflect.defineProperty(target, CALL_CHAIN, {
+    configurable: false,
+    writable: false,
+    enumerable: false,
+    value: chain,
+  });
   return new Proxy(target, new ChainProxyHandler(createProxy)) as any;
 }
 function getLinkPath(node: Chian) {
@@ -58,19 +94,17 @@ function flipArray(arr: any[]): void {
 }
 const CALL_CHAIN = Symbol("call chain");
 
-export type ChianProxy<T extends object, E extends object = {}> = E & {
-  [Key in keyof T as T[Key] extends object ? Key : never]: CallChianProxy<T[Key], E>;
+/** 对象链 */
+export type ObjectChian = {
+  [key: string | number | symbol]: ObjectChian;
+};
+/** 可调用的对象链 */
+export type CallChian<T extends Fn> = {
+  (...args: Parameters<T>): ReturnType<T>;
+  [key: string | number | symbol]: CallChian<T>;
 };
 
 type Fn = (...args: any[]) => any;
-export type CallChianProxy<T, E extends object = {}> = T extends Fn
-  ? MakeAsync<T> & ChianProxy<T, E>
-  : T extends object
-  ? ChianProxy<T, E>
-  : never;
-
-type MakeAsync<T> = T extends Fn ? (...args: Parameters<T>) => ToPromise<ReturnType<T>> : never;
-type ToPromise<T> = T extends Promise<any> ? T : Promise<T>;
 
 type Chian = {
   next?: Chian;
