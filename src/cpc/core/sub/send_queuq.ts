2ba78c12f2560f07c96ebc40fc70bc4a787ef165
Eavid <ganrn@vip.qq.com>
2024-05-24 16:37:01 +0800

    test: 覆盖率测试包含文件更改

--- src/cpc/core/sub/send_queuq.ts
+++
@@ -1,9 +0,0 @@
-import { RpcFrame } from "../type.ts";
-
-class SendingQueue {
-  queue: RpcFrame[] = [];
-  constructor(senFrame: (frame: RpcFrame[]) => Promise<void>) {}
-  push(frame: RpcFrame) {
-    this.queue.push(frame);
-  }
-}
