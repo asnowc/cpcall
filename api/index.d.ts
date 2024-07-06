546ae64e170146e09e79aba65a98434f8c099aa6
Eavid <eavidy@qq.com>
2024-07-06 15:01:38 +0800

    build: 目录调整

--- api/index.d.ts
+++
@@ -1,8 +0,0 @@
-/**
- * api抽取
- */
-
-import * as core from "../dist/cpc.js";
-import * as node from "../dist/node.js";
-import * as web from "../dist/web.js";
-export { core, web, node };
---
+++ build/api_extract/index.d.ts
@@ -0,0 +1,7 @@
+/**
+ * api抽取
+ */
+
+export * as core from "../../dist/cpc.js";
+export * as node from "../../dist/node.js";
+export * as web from "../../dist/web.js";
